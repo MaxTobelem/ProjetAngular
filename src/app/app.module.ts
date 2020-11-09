83af5d4f9c02b10056ea4e8b588adcdb70762ac2
Max TOBELEM <maxtobelem@MacBook-Pro-de-Max.local>
2020-11-09 01:27:35 +0100

    Fin graphique

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,18 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatMenuModule} from '@angular/material/menu';
+import {MatInputModule} from '@angular/material/input';
 import { MapComponent } from './map/map.component';
 import { ChartsComponent } from './charts/charts.component';
 import { AboutComponent } from './about/about.component';
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 import { HttpClientModule } from '@angular/common/http';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import * as PlotlyJS from 'plotly.js/dist/plotly.js';
+import { PlotlyModule } from 'angular-plotly.js';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+PlotlyModule.plotlyjs = PlotlyJS;
 
 @NgModule({
   declarations: [
@@ -33,7 +39,11 @@ import { FlexLayoutModule } from '@angular/flex-layout';
     MatMenuModule,
     LeafletModule,
     HttpClientModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    PlotlyModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/charts/charts.component.ts
+++ src/app/charts/charts.component.ts
@@ -1,15 +1,86 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-charts',
   templateUrl: './charts.component.html',
   styleUrls: ['./charts.component.scss']
 })
-export class ChartsComponent implements OnInit {
+export class ChartsComponent  {
+  public nbEntree: number;
+  public allData: any[];
+  public layout: object;
+  public config: object;
+  public compteur: 0;
+  selectedValue: string;
 
-  constructor() { }
+  departementForm = this.formBuilder.group({
+    departement: '' ,
+  });
 
-  ngOnInit() {
+  constructor(
+      private http: HttpClient,
+      private formBuilder: FormBuilder,
+  ) {
+
+      this.nbEntree = 0;
+      this.allData = [];
+      this.allData.push({
+          type: 'scatter',
+          x: [],
+          y: [],
+          marker: {
+              color: 'red'
+          },
+          name: 'Prix (€)',
+          legendgroup: '0',
+      });
+      this.layout = {
+          title: 'Prix moyen au m2',
+          xaxis: {                  // all "layout.xaxis" attributes: #layout-xaxis
+          visible: false,
+         },
+          autosize: true,
+          mode: 'lines+markers',
+      };
+      this.config = {
+          responsive: true
+      };
+
+      this.loadData();
   }
 
+
+  public loadData(): void {
+      console.log(this.departementForm.value.departement);
+      this.http.get('assets/data.csv', { responseType: 'text' })
+          .subscribe(data => {
+              this.parseXmlFile(data, this.departementForm.value.departement);
+          });
+  }
+
+
+  private parseXmlFile(csvContent: string, dep: string): void {
+      const csvContentByLine = csvContent.split('\n');
+
+      const xTempDep: string[] = [];
+      const yTempPrix: number[] = [];
+
+      csvContentByLine.forEach((csvLine) => {
+          // Verif ligne non vide, inséré par Pandas
+          if (csvLine.length && csvLine !== '') {
+              const currentLine = csvLine.split(';');
+              if (currentLine[0] === dep){
+              xTempDep.push(String(currentLine[1]));
+              yTempPrix.push(Number(currentLine[4]));
+              this.nbEntree++;
+              this.compteur++;
+            }
+          }
+      });
+
+      this.allData[0].x = xTempDep;
+      this.allData[0].y = yTempPrix;
+  }
 }
